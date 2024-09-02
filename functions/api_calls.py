2e4c820ae32e9a5accca044c5cda82db4d0522e4
Gus1101 <augustosantosb60@gmail.com>
2024-09-02 17:19:52 -0300

    update api_calls.py to explode and concatenate data

--- functions/api_calls.py
+++ functions/api_calls.py
@@ -3,20 +3,17 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 import pandas as pd
-import pytest
 
-# Variáveis de ambeinte
+# Variáveis de ambiente
 caminho_env = 'c:/Users/Augusto/Desktop/FIAP/enterprise_challange/config/.env'
 load_dotenv(caminho_env)
 
 API_KEY = os.getenv('API_KEY')
 
 def gerar_datas_ate(data_final):
-
     """
     Função para gerar uma lista com datas no ano de 2024
     """
-
     datas = []
     data_atual = datetime(2024, 1, 1).date()
     while data_atual <= data_final:
@@ -24,13 +21,10 @@ def gerar_datas_ate(data_final):
         data_atual += timedelta(days=1)
     return datas
 
-
 def requisitar_dados(data):
-
     """
     Função para realizar requisições em uma API
     """
-    
     url = f'http://api.weatherapi.com/v1/history.json?key={API_KEY}&q=Sao Paulo&dt={data}'
     resposta = requests.get(url)
     if resposta.status_code == 200:
@@ -39,7 +33,7 @@ def requisitar_dados(data):
         print(f'Erro na requisição para {data}: {resposta.status_code}')
         return None
 
-# Gera a lista de datas até data atual
+# Gera a lista de datas até data final
 data_final = datetime(2024, 6, 30).date()
 datas = gerar_datas_ate(data_final)
 
@@ -53,10 +47,21 @@ for data in datas:
 # Converter a lista de dicionários em um DataFrame
 df = pd.json_normalize(resultados)
 
+# Normalizar a parte 'forecastday' dentro de 'forecast'
+df_forecast = pd.json_normalize(df['forecast.forecastday'].explode())
+
+# Normalizar a parte 'hour' dentro de 'forecastday'
+if 'hour' in df_forecast.columns:
+    df_hour = pd.json_normalize(df_forecast['hour'].explode())
+    df_forecast = df_forecast.drop(columns=['hour']).join(df_hour, rsuffix='_hour')
+
+# Concatenar com o DataFrame principal
+df_final = pd.concat([df.drop(columns='forecast.forecastday'), df_forecast], axis=1)
+
 # Caminho para salvar o arquivo .parquet
-caminho_parquet = 'c:/Users/Augusto/Desktop/FIAP/enterprise_challange/resultado.parquet'
+caminho_parquet = 'c:/Users/Augusto/Desktop/FIAP/enterprise_challange/data/weather_sp.parquet'
 
 # Salvar o DataFrame como um arquivo .parquet
-df.to_parquet("C:/Users/Augusto/Desktop/FIAP/enterprise_challange/data/wheater_sp.parquet", engine='pyarrow')
+df_final.to_parquet(caminho_parquet, engine='pyarrow')
 
-print(f'DataFrame salvo em {"data/data"}')
+print(f'DataFrame salvo em {caminho_parquet}')
